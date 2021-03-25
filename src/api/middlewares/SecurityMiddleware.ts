a7cc38c177e02a0ba7aafd46901d16ffa2c8b8d9
Patrice Laplante <plaplante@ferlab.bio>
2021-03-25 09:38:13 -0400

    fix: #269 attempt to fix connection lost

--- src/api/middlewares/SecurityMiddleware.ts
+++ src/api/middlewares/SecurityMiddleware.ts
@@ -1,9 +1,9 @@
-// import { ExpressMiddlewareInterface/*, Middleware*/ } from 'routing-controllers';
+import { ExpressMiddlewareInterface, Middleware } from 'routing-controllers';
 import * as express from 'express';
 import helmet from 'helmet';
 
-// @Middleware({ type: 'before' })
-export class SecurityMiddleware /*implements ExpressMiddlewareInterface*/ {
+@Middleware({ type: 'before' })
+export class SecurityMiddleware implements ExpressMiddlewareInterface {
     public use(req: express.Request, res: express.Response, next: express.NextFunction): any {
         return helmet({
             contentSecurityPolicy: false,
