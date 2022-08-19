a8ca6382ff6245209ed34f0c60e3bc56247f4f8d
Aymeric Toulouse <atoulouse@caresimple.com>
2022-08-18 21:10:29 -0400

    fix: CQDG-00 - add script to modify column name submitter_donor_id to submitter_participant_id in sample_registration table

---
+++ src/database/migrations/1660871196-ModifyColumnName.ts
@@ -0,0 +1,15 @@
+import {MigrationInterface, QueryRunner} from 'typeorm';
+
+export class ModifyColumnName1660871196 implements MigrationInterface {
+    private tableName = 'sample_registration';
+    private oldColumnName = 'submitter_donor_id';
+    private newColumnName = 'submitter_participant_id';
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.renameColumn(this.tableName, this.oldColumnName, this.newColumnName);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.renameColumn(this.tableName, this.newColumnName, this.oldColumnName);
+    }
+}
